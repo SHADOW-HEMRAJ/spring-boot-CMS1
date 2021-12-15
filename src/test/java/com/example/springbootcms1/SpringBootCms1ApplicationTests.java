0fd395cd69c9b333f59b3c0e8ae9fa8fdcec5c93
DESKTOP-OG94NNN\Admin <namitagothwal99@gmail.com>
2021-12-15 18:21:37 +0530

    added full project

---
+++ src/test/java/com/example/springbootcms1/SpringBootCms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
